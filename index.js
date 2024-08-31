72940f4eb840aa1a00b2aab89657c8007d7bcff2
Coolz <coolzisgood@gmail.com>
2024-08-31 13:12:08 -0700

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Watching Coolz Development, LLC"];
 
 
 let currentIndex = 0;
